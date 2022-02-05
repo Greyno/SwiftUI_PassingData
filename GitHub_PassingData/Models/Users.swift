0681858d1d5192f9281533c0aa574568db5041a5
G R-Titko <Greyno@users.noreply.github.com>
2022-02-05 10:40:50 -0500

    Update Users.swift

--- GitHub_PassingData/Models/Users.swift
+++ GitHub_PassingData/Models/Users.swift
@@ -14,39 +14,71 @@ class Users: ObservableObject {
 
     var userStore: [User] { usersCache }
 
+    //Load/save files from/to the Document directory, instead of from the app bundle
+   let usersJsonUrl = URL(fileURLWithPath: "UserNames", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
+    
     @Published var usersCache = [User]() {
         didSet {
-            guard let data = try? JSONEncoder().encode(usersCache.self) else { return }
-            UserDefaults.standard.set(data, forKey: "Users")
-            for user in usersCache {
-                print("The saved user is \(user.firstName) \(user.lastName)")
-            }
+            saveUserNames()
         }
     }
     
     init() {
 
-        if let users = UserDefaults.standard.data(forKey: "Users") {
-            if let decoded = try? JSONDecoder().decode([User].self, from: users) {
-                self.usersCache = decoded
-                return
-            }
-        }
-        self.usersCache = []
+        loadUserNames()
         }
     
+    private func saveUserNames() {
+        //Create a Json encoder object
+        let jsonEncoder = JSONEncoder()
+        jsonEncoder.outputFormatting = .prettyPrinted //Make the JSON file more readable
+        
+        //Encode the Swift type into Data then save to the Document Directory URL
+        do {
+            let usersData = try jsonEncoder.encode(usersCache)
+            try usersData.write(to: usersJsonUrl, options: .atomicWrite)
+        } catch  {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    private func loadUserNames() {
+        print("Document directory url: \(FileManager.documentsDirectoryURL.absoluteURL)")
+        //Gracefully handle having no PList file in the documents directory
+        guard FileManager.default.fileExists(atPath: usersJsonUrl.path) else { return }
+        
+        //Create a JSON decoder object
+        let jsonDecoder = JSONDecoder()
+        
+        //Load contents of the PList (from the Document directory URL) as Swift Data, then decode these Data objects to a Swift type
+        do {
+            let userNamesData = try Data(contentsOf: usersJsonUrl)
+            usersCache = try jsonDecoder.decode([User].self, from: userNamesData)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     func addUser(user: User) {
-        print("Running the user is add function")
         usersCache.insert(user, at: 0)
+        
+        for user in usersCache {
+            print("The first user is \(user.firstName) \(user.lastName)")
+            
+        }
+        //print("The first user is \(usersCache[0].firstName) \(usersCache[0].lastName)")
     }
     
     func deleteUser(at offsets: IndexSet) {
         usersCache.remove(atOffsets: offsets)
     }
     
+//TODO: Add UTs to ensure this function replaces the item expected
     func update(userToChange: User) {
         let foundIndex = usersCache.firstIndex { (user) -> Bool in
-            user.firstName == userToChange.firstName && user.lastName == userToChange.lastName
+            user == userToChange
+            //user.firstName == userToChange.firstName && user.lastName == userToChange.lastName
         }
         usersCache[foundIndex!] = userToChange
     }
